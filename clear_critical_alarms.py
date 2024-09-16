aaa7eaa63906edacd7cbe610b57c00c0c9ce9c9f
ercintorun <ercintorun@gmail.com>
2024-09-16 13:37:32 +0300

    Update clear_critical_alarms.py

--- clear_critical_alarms.py
+++ clear_critical_alarms.py
@@ -1,9 +1,13 @@
 import mechanicalsoup
 import paramiko
+import time
 
+##Aircon Login Credentials
 username = "username"
 password = "password"
 webpage= "http://192.168.9.8"
+
+##Open Aircon Webpage and login
 
 browser = mechanicalsoup.StatefulBrowser(
     soup_config={'features': 'lxml'},
@@ -19,23 +23,36 @@ browser["login_password"] = password
 resp = browser.submit_selected()
 
 
-# verify we are now logged in
+## verify we are now logged in
+
 page = browser.page
 messages = page.find("div", class_="app-secondary-name")
 if messages:
-	print ("login successful, the new url is: " + browser.url)
-	
+        print ("login successful, the new url is: " + browser.url)
+
 # check the alarm-icon-section division
-	alarmcount = page.find("div", id="alarm-icon-section")
-	if alarmcount: 
-		alarmtext=str(alarmcount)
-		total_alarms= alarmtext.split('title="')[-1].split("present")[0]
-		print (total_alarms)
-		if int(total_alarms[0]) > 0:
-			print ("Critical alarm exist")
-			browser.select_form('form[name="HashForm1"]')
-			browser.form.set("ClearActiveAlarms",True)
-			print ("--The form values below selected, checkbox tick--")
-			browser.form.print_summary()	
-			resp = browser.submit_selected()
-			print("------Alarms Cleared------")
+        alarmcount = page.find("div", id="alarm-icon-section")
+        if alarmcount:
+                alarmtext=str(alarmcount)
+                total_alarms= alarmtext.split('title="')[-1].split("present")[0]
+                print (total_alarms)
+                if int(total_alarms[0]) > 0:
+                        ###
+                        #A function to record output to a file when printing to screen also
+                        timestr = time.strftime("%Y%m%d-%H%M%S")
+                        outputfile= "aircon_critical_alarm.txt"
+                        OF = open(outputfile, 'a')
+
+                        def printing(text):
+                                print (str(text))
+                                if outputfile:
+                                        OF.write(text + "\n")
+                        ###
+                        printing ("-------"+timestr+" "+webpage+"-------")
+                        printing (str(total_alarms))
+                        browser.select_form('form[name="HashForm1"]')
+                        browser.form.set("ClearActiveAlarms",True)
+                        print ("--The form values below selected, checkbox tick--")
+                        browser.form.print_summary()
+                        resp = browser.submit_selected()
+                        printing("Alarms Cleared")
